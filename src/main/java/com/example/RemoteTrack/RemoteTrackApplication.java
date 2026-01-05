a468bd3867f0603f4577d564e9658f1f6e1ed155
pankaj <pankajsemwalpankaj123@gmail.com>
2026-01-05 14:36:44 +0530

    initalized repo

---
+++ src/main/java/com/example/RemoteTrack/RemoteTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.RemoteTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteTrackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/RemoteTrack/entity/JobApplication.java
@@ -0,0 +1,5 @@
+package com.example.RemoteTrack.entity;
+
+
+public class JobApplication {
+}
