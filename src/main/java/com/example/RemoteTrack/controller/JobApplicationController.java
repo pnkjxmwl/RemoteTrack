0fe487f934088f04ebed7b2a91bd2ce733df9264
pankajsemwal <pankajsemwalpankaj123@gmail.com>
2026-04-08 20:33:35 +0530

    added all origins

--- src/main/java/com/example/RemoteTrack/controller/JobApplicationController.java
+++ src/main/java/com/example/RemoteTrack/controller/JobApplicationController.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class JobApplicationController {
